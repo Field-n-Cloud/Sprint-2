2aeb9398a6631ba09ecb7cea8d00ae51a1f9f91a
tialvesdev <tialves.dev@gmail.com>
2023-05-22 15:24:34 -0300

    Cadastro unificado em um arquivo, front feito e back feito até o models

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -72,21 +72,22 @@ function entrar(req, res) {
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var nome = req.body.nomeServer;
-    var email = req.body.emailServer;
-    var senha = req.body.senhaServer;
+    var clube = req.body.clubeServer;
+    var endereco = req.body.enderecoServer;
+    var estadio = req.body.estadioServer;
+    var gramado = req.body.gramadoServer;
 
     // Faça as validações dos valores
-    if (nome == undefined) {
-        res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está undefined!");
-    } else {
+    // if (nome == undefined) {
+    //     res.status(400).send("Seu nome está undefined!");
+    // } else if (email == undefined) {
+    //     res.status(400).send("Seu email está undefined!");
+    // } else if (senha == undefined) {
+    //     res.status(400).send("Sua senha está undefined!");
+    // } else {
         
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(clube, edereco, estadio, gramado)
             .then(
                 function (resultado) {
                     res.json(resultado);
@@ -101,7 +102,7 @@ function cadastrar(req, res) {
                     res.status(500).json(erro.sqlMessage);
                 }
             );
-    }
+    // }
 }
 
 function cadastrarClube(req, res) {
--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -28,16 +28,28 @@ function entrar(email, senha) {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucao
-function cadastrar(nome, email, senha) {
+function cadastrar(clube, endereco, estadio, gramado) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
-    var instrucao = `
+    var instrucao1 = `
+        INSERT INTO clube (nomeClube, cnpjClube, senhaClube, emailClube, telefoneClube, ufClube) VALUES ('${clube.nomeClube}', '${clube.cnpjClube}', '${clube.senhaClube}', '${clube.emailClube}', '${clube.telefoneClube}', '${estadoClube}');
+    `;
+
+    var instrucao2 = `
+        INSERT INTO endereco (cep, logradouro, numero, complemento) VALUES ('${endereco.cepEndereco}', '${endereco.logradouroEndereco}', '${endereco.numeroEndereco}', '${endereco.complementoEndereco}');
+    `;
+
+    var instrucao3 = `
+        INSERT INTO estadio (nome, email, senha) VALUES ('${nome}', '${email}', '${senha}');
+    `;
+
+    var instrucao4 = `
         INSERT INTO usuario (nome, email, senha) VALUES ('${nome}', '${email}', '${senha}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    return [database.executar(instrucao1), database.executar(instrucao2), database.executar(instrucao3), database.executar(instrucao4)];
 }
 
 function cadastrarClube(nome, email, senha, cnpj, telefone, uf) {
